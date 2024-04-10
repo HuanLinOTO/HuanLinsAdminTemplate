756bd362663645f9ce072136c187da18a81857e5
HuanLinOTO <linsmc@126.com>
2024-04-10 18:11:57 +0800

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>
-    
+  <div style="height: 100vh">
+    <n-message-provider>
+      <n-layout has-sider style="height: 100vh">
+        <n-layout-sider v-if="show_aside_bar">
+          <Menu />
+        </n-layout-sider>
+        <n-layout>
+          <n-layout-header>
+            <div class="page-title">
+              {{ page_title }}
+            </div>
+          </n-layout-header>
+          <n-layout-content content-style="padding: 24px;">
+            <RouterView></RouterView> 
+          </n-layout-content>
+        </n-layout>
+      </n-layout>
+    </n-message-provider>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useStore } from './store';
+import Menu from './components/Menu.vue';
+const store = useStore()
+
+const show_aside_bar = ref(true)
+const page_title = ref("PageTitle")
+
+// show_aside_bar.value = store.isLogin
+
+// watch(() => store.isLogin, (val) => {
+//   show_aside_bar.value = val
+// })
 </script>
 
 <style lang="less" scoped>
-
+.page-title {
+  font: 24px bold;
+  padding: 8px;
+}
 </style>
---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <n-menu :options="menuOptions" @update:value="handleUpdateValue" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import { defineComponent, h, Component } from 'vue'
+import { NIcon, useMessage } from 'naive-ui'
+import type { MenuOption } from 'naive-ui'
+import { RouterLink } from 'vue-router'
+import {
+  BookOutline as BookIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+  HomeOutline as HomeIcon
+} from '@vicons/ionicons5'
+
+const message = useMessage()
+
+function renderIcon (icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = ref<MenuOption[]>([
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: 'index'
+          }
+        },
+        { default: () => 'WebsiteTitle' }
+      ),
+    key: 'go-back-home'
+  },
+  {
+    key: 'divider-1',
+    type: 'divider',
+    props: {
+      style: {
+        marginLeft: '32px'
+      }
+    }
+  },
+  {
+    label: () =>
+      h(
+        'a',
+        {
+          href: 'https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F',
+          target: '_blank',
+          rel: 'noopenner noreferrer'
+        },
+        '且听风吟'
+      ),
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  }
+])
+
+const handleUpdateValue = (key: string, item: MenuOption) => {
+}
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
